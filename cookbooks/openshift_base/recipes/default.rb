9c2bdf6050c66424fd82ed099fa7daa2ffc513af
Dan Scarf <dscarf@gmail.com>
2018-08-21 19:37:44 -0700

    Adding resource to install some packages as required for host preparation.

--- cookbooks/openshift_base/recipes/default.rb
+++ cookbooks/openshift_base/recipes/default.rb
@@ -3,3 +3,9 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+['wget', 'git', 'net-tools', 'bind-utils', 'yum-utils', 'iptables-services', 'bridge-utils', 'bash-completion', 'kexec-tools', 'sos', 'psacct'].each do |p|
+    package p do
+      action :install
+    end
+  end
