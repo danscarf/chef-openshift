1f7f44da7393baf8dc09bb76c05969d4a08641b5
Dan Scarf <dscarf@gmail.com>
2018-11-02 19:44:16 -0700

    Adding user.

--- cookbooks/openshift_base/recipes/default.rb
+++ cookbooks/openshift_base/recipes/default.rb
@@ -16,10 +16,25 @@
     action :nothing
   end
 
-#  docker_installation_package 'default' do
-#  end
-
   docker_service 'default' do
     insecure_registry '172.30.0.0/16'
     action [:create, :start]
+  end
+
+  user 'openshift' do
+    comment 'openshift user'
+    manage_home true
+    shell '/bin/bash'
+    home '/home/openshift'
+    password '$1$Rfp/kEGM$oTlULpdHmr/ZA0Vjnk9LN1'
+  end
+  
+  sudo 'admin' do
+    user 'openshift'
+  end
+
+  group 'docker' do
+    action :modify
+    members 'openshift'
+    append true
   end
